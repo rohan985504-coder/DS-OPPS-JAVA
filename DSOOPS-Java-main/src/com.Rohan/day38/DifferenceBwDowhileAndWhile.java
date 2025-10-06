097d03a085e76df114f76c6de85231dc6047abea
rohan985504-coder <rohan0698.becse24@chitkara.edu.in>
2025-10-06 10:18:59 +0530

    Update DifferenceBwDowhileAndWhile.java

--- DSOOPS-Java-main/src/com.Rohan/day38/DifferenceBwDowhileAndWhile.java
+++ DSOOPS-Java-main/src/com.Rohan/day38/DifferenceBwDowhileAndWhile.java
@@ -1,11 +1,3 @@
-package com.Rahul.day38;
-
-//Explain the difference between while and do-while loops with code
-//examples.
-
-// Answer the while loop firstly checks the condition and then executes the block of code
-// Do-while loop executes the code once and then check the condition for executing further
-
 public class DifferenceBwDowhileAndWhile {
     public static void main(String[] args) {
         System.out.println("While loop: ");
