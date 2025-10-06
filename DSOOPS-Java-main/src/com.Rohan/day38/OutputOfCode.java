e057faff48830eac4ce9ddaad8ea47a2c3b93a6a
rohan985504-coder <rohan0698.becse24@chitkara.edu.in>
2025-10-06 10:20:01 +0530

    Update OutputOfCode.java

--- DSOOPS-Java-main/src/com.Rohan/day38/OutputOfCode.java
+++ DSOOPS-Java-main/src/com.Rohan/day38/OutputOfCode.java
@@ -1,9 +1,3 @@
-package com.Rahul.day38;
-
-//What is the output of the following loop:
-
-// Answer : 0 2
-
 public class OutputOfCode {
     public static void main(String[] args) {
         for(int i = 0;i<3;i++){
